5ee63844f18beeacc7d6d0add867677f9889aa77
Radist-electric <radist-electric@yandex.ru>
2020-04-27 22:11:47 +0500

    Yura! index.js is for you

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-// Hello everybody!!! test2 20200427 22:05
+// Hello Yura!!! test2 20200427 22:05
 // This is test2
